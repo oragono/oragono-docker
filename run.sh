6d29a976210b1a638567c9a6b4a5b91c6a82ab99
Daniel Oaks <daniel@danieloaks.net>
2019-02-03 08:14:23 +1000

    change oper password on launching the server for the first time

--- run.sh
+++ run.sh
@@ -6,14 +6,28 @@ cd /ircd
 # make config file
 if [ ! -f "/ircd/ircd.yaml" ]; then
     awk '{gsub(/path: languages/,"path: /ircd-bin/languages")}1' /ircd-bin/oragono.yaml > /tmp/ircd.yaml
-    mv /tmp/ircd.yaml /ircd/ircd.yaml
+
+    # change default oper passwd
+    OPERPASS=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c20)
+    echo "Oper username:password is dan:$OPERPASS"
+    ENCRYPTEDPASS=$(echo "$OPERPASS" | /ircd-bin/oragono genpasswd)
+    ORIGINALPASS='\$2a\$04\$LiytCxaY0lI.guDj2pBN4eLRD5cdM2OLDwqmGAgB6M2OPirbF5Jcu'
+
+    awk "{gsub(/password: \\\"$ORIGINALPASS\\\"/,\"password: \\\"$ENCRYPTEDPASS\\\"\")}1" /tmp/ircd.yaml > /tmp/ircd2.yaml
+
+    unset OPERPASS
+    unset ENCRYPTEDPASS
+    unset ORIGINALPASS
+
+    mv /tmp/ircd2.yaml /ircd/ircd.yaml
 fi
 
-# make db and certs
+# make db
 if [ ! -f "/ircd/ircd.db" ]; then
     /ircd-bin/oragono initdb
 fi
 
+# make self-signed certs
 if [ ! -f "/ircd/tls.key" ]; then
     /ircd-bin/oragono mkcerts
 fi
